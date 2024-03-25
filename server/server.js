bd0fc55c58fc56a15f07ba1e9a657e27cdc49f99
Haseebz1 <haseebkandore@hotmail.com>
2024-03-25 12:14:31 +0000

    Midleware , db and routes added

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
+
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection')
-
+const db = require('./config/connection');
+const routes = require('./routes/routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
 db.once('open', () => {
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  console.log('Connected to MongoDB');
+  // Use routes
+  app.use('/api', routes);
+
+  // Start the server
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
